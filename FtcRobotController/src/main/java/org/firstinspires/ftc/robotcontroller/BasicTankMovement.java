2f83efea5f7f6709caf172a27d45801d373f3f22
ShermWorm08 <CMSherman08@Gmail.com>
2024-10-31 15:46:27 -0400

    pookie

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/BasicTankMovement.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/BasicTankMovement.java
@@ -3,7 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+//pookie
     @TeleOp(name="Basic Tank Movement", group="Linear OpMode")
 
     public class BasicTankMovement extends LinearOpMode {
